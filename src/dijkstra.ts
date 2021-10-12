6072cbbc9eb23ddcc2ccf989656cf5eaf073bd76
Kyle Pfromer <kpfromer@kpfromer-mac.local>
2021-10-11 22:07:06 -0600

    feat: add full dijkstra example

---
+++ src/dijkstra.ts
@@ -0,0 +1,67 @@
+import Graph from 'node-dijkstra';
+import { table as tablePretty } from 'table';
+
+export type Table = string;
+
+export type GraphRaw = [node: string, neighbors: Record<string, number>];
+export type GraphMap = Map<string, Map<string, number>>;
+
+export function toGraphMap(raw: GraphRaw[]): GraphMap {
+  const graph = new Map();
+
+  raw.forEach(([node, neighbors]) => {
+    const neighborsMap = Object.entries(neighbors).reduce((map, [neighbor, weight]) => {
+      map.set(neighbor, weight);
+      return map;
+    }, new Map());
+
+    graph.set(node, neighborsMap);
+  });
+
+  return graph;
+}
+
+function toGraph(map: GraphMap): Graph {
+  return new Graph(map as any);
+}
+
+function nodeNames(graph: GraphMap): string[] {
+  return Array.from(graph.keys());
+}
+
+export function toTable(graph: GraphMap, distance = Infinity): Table[] {
+  const tables: Table[] = [];
+
+  const nodes = nodeNames(graph);
+
+  for (const src of nodes) {
+    const table: string[][] = [['Dest', 'Cost', 'Hop']];
+
+    for (const dest of nodes) {
+      if (src !== dest) {
+        const g = toGraph(graph);
+
+        const { path, cost } = g.path(src, dest, { cost: true });
+        if (path) {
+          const nextHop = path.length > 1 ? path[1] : path[0];
+          table.push([dest, cost, nextHop]);
+        } else {
+          table.push([dest, '-', '-']);
+        }
+      } else {
+        table.push([src, '0', src]);
+      }
+    }
+
+    tables.push(
+      tablePretty(table, {
+        header: {
+          alignment: 'center',
+          content: `Table for src "${src}"`,
+        },
+      }),
+    );
+  }
+
+  return tables;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,70 @@
 #!/bin/env node
 import * as yargs from 'yargs';
 import * as packageJson from '../package.json';
+import { GraphRaw, toGraphMap, toTable } from './dijkstra';
+import { table as tablePretty } from 'table';
+
+const nodes: GraphRaw[] = [
+  [
+    'A',
+    {
+      C: 2,
+      B: 5,
+    },
+  ],
+  [
+    'B',
+    {
+      A: 5,
+      D: 2,
+    },
+  ],
+  [
+    'C',
+    {
+      A: 2,
+      E: 2,
+      D: 8,
+    },
+  ],
+  [
+    'D',
+    {
+      B: 2,
+      C: 8,
+      F: 4,
+    },
+  ],
+  [
+    'E',
+    {
+      C: 2,
+      F: 4,
+    },
+  ],
+  [
+    'F',
+    {
+      D: 4,
+      E: 4,
+    },
+  ],
+];
 
 yargs
+  .command(
+    'run',
+    'print a test message',
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    () => {},
+    () => {
+      const graph = toGraphMap(nodes);
+
+      const tables = toTable(graph);
+
+      console.log(tables.join('\n'));
+    },
+  )
   .command(
     'do-something',
     'print a test message',
